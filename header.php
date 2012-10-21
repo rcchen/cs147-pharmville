bfd4be262c4f11ea5f825d52337bf8a676fcd87e
Severus Snape <severus@mysteries.stanford.edu>
2012-10-21 13:55:23 -0700

    Basic things from severus

--- header.php
+++ header.php
@@ -1,6 +1,19 @@
-<?
-    
-    // Include necessary stuff
-    include_once('config.php');
+<head>
+    <? include_once('config.php'); ?>
+    <link rel="stylesheet" type="text/css" href="css/cssreset-min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/zepto.min.js"></script>
+</head>
 
-?>
+<!-- BEGIN HEADER -->
+<div class="header">
+	<div class="header-contents">Pharmville (click to reload)</div>
+</div>
+<!-- END HEADER -->
+
+<script>
+	var header = $('.header-contents')
+	header.on('click', function(e) {
+		window.location.reload()
+	})
+</script>
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-
     include_once('header.php');
+?>
+
+<div class="foo">
+    <?
+        $result = $mysqli->query("SELECT * FROM books");
+        while ($row = $result->fetch_assoc()) { ?>
+        	<div class="book">
+        		<div class="book-title"><? echo $row['title']; ?></div>
+        		<div class="book-author"><? echo $row['author']; ?></div>
+        		<div class="book-stock <?
+        			if ($row['inventory'] > 0) {
+  						printf('book-instock">In stock (%d)', $row['inventory']);
+        			} else {
+        				echo 'book-nostock">Out of stock';
+        			}
+        		?></div>
+        	</div>
+        <? }
+    ?>
+</div>
