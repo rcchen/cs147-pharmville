ed5d23dd08ed9a369bb4a0d470b3dc7cfe6a86e6
Roger Chen <rogerthechen@gmail.com>
2012-10-20 20:50:49 -0700

    Attempting to fix up issues with submoduling the iPhone application

---
+++ iPhoneApp/TestSim/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSim
+//
+//  Created by Roger Chen on 10/8/12.
+//  Copyright (c) 2012 Roger Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPhoneApp/TestSim/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestSim
+//
+//  Created by Roger Chen on 10/8/12.
+//  Copyright (c) 2012 Roger Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
