e44a56913357d17bae7c44ba41968e73e6c64ce7
Severus Snape <severus@mysteries.stanford.edu>
2012-10-25 13:55:34 -0700

    Fixed scrolling issue with sticky footer

--- medications.php
+++ medications.php
@@ -3,7 +3,7 @@
 	<div id="medications-container" class="container">
 		<div id="medications-title">Just a couple of medications. These are being pulled from mySQL md_medicationdata table.</div>
 		<?
-			$result = $mysqli->query("SELECT * FROM md_medicationdata LIMIT 3");
+			$result = $mysqli->query("SELECT * FROM md_medicationdata LIMIT 10");
 			while ($row = $result->fetch_assoc()) {?>
 				<div class="medication">
 					<div class="medication-brand"><? echo $row['mdataBrand']; ?></div>
