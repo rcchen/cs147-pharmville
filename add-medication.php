6dc4b6696b02640c3564a2c22472fa2789cced29
Severus Snape <severus@mysteries.stanford.edu>
2012-10-26 13:00:44 -0700

    Changes to the medications file

---
+++ add-medication.php
@@ -0,0 +1,5 @@
+<? include_once('header.php'); ?>
+<div id="home" class="content">
+	This is the screen to add a new medication.
+</div>
+<? include_once('footer.php'); ?>
--- medications.php
+++ medications.php
@@ -2,15 +2,27 @@
 <div id="medications" class="content">
 	<div id="medications-container" class="container">
 		<div id="medications-title">Just a couple of medications. These are being pulled from mySQL md_medicationdata table.</div>
+		<a class="button" id="add-med-button" href="add-medication.php">Add New Medication</a>
+		<table class="medications-table">
 		<?
-			$result = $mysqli->query("SELECT * FROM md_medicationdata LIMIT 10");
+			$result = $mysqli->query("SELECT * FROM md_medicationdata");
 			while ($row = $result->fetch_assoc()) {?>
-				<div class="medication">
+				<tr class="medication"><td>
+				<a href="medication.php?id=<? echo $row['mdataId']; ?>">
 					<div class="medication-brand"><? echo $row['mdataBrand']; ?></div>
+
 					<div class="medication-description"><? echo $row['mdataCategory']; ?></div>
-				</div>
+				</a>
+				</td><td>
+				<div class="medication-alarm">
+					<select id="toggleswitch1" data-theme="b" data-role="slider" >
+            			<option value="off">Off</option>
+                		<option value="on">On</option>
+           		 	</select>
+				</div></td></tr>
 			<?}
 		?>
+		</table>
 	</div>
 </div>
 <? include_once('footer.php'); ?>
