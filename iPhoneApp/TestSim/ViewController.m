0001f61d505e02fd0952dec9a33edc7e6200c97c
Roger Chen <rogerthechen@gmail.com>
2012-10-29 09:17:47 -0700

    Committing before pull

--- iPhoneApp/TestSim/ViewController.m
+++ iPhoneApp/TestSim/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSString *site = @"http://corgiland.com/cs147/";
+    NSString *site = @"http://cs147.adamantinelabs.com/";
     NSURL *url = [NSURL URLWithString:site];
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
     
