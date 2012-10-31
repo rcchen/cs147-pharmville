291190af520d7804f22990d63d54b6355b06354e
Tiffany Dharma <tdharma@stanford.edu>
2012-10-31 13:43:50 -0700

    Added circles to CSS stylesheet

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <? include_once('header.php'); ?>
 <div data-role="page">
 	<div data-role="header" data-tap-toggle="false">
-		<h1>Home</h1>
+		<h1>Welcome Home!</h1>
 	</div><!-- /header -->
-	<div data-role="content" class="sp-content" id="home">	
-		<p>Hello world</p>	
-		<div class="sp-circle-main">
-			<div class="sp-circle-text">Testing</div>
+	<div data-role="content" class="sp-content" id="home">
+		<div class="sp-circle-small">Tylenol</div>
+		<div class="sp-circle-med">Zocor</div>
+		<div class="sp-circle-main">12:33PM</div>
+		<div class="sp-circle-med">Zoloft</div>
+		<div class="sp-circle-small">Vicodin</div>
 		</div>	
 	</div><!-- /content -->
 <? include_once('footer.php'); ?>
