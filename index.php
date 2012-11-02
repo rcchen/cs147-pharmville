c98523cbe62f2221fe0160a109460acd0420f555
Tiffany Dharma <tdharma@stanford.edu>
2012-11-01 18:31:53 -0700

    Fixed index clock

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<div data-role="header" data-tap-toggle="false">
 		<h1>Home</h1>
 	</div><!-- /header -->
-	<div data-role="content" class="sp-content" id="home">
+	<div data-role="content" class="sp-content">
 		<div class="sp-circle-small"><br><br>Tylenol<br>2:15PM</div>
 		<div class="sp-circle-med"><br><br>Zocor<br>1:00PM</div>
 		<div class="sp-circle-main">
@@ -14,15 +14,16 @@
 	var hours = currentTime.getHours()
 	var minutes = currentTime.getMinutes()
 	if (minutes < 10){
-	minutes = "0" + minutes
+		minutes = "0" + minutes
 	}
-	if(hours >= 13)
-		hours = hours - 12;
-	document.write(hours + ":" + minutes + " ")
-	if(hours > 11){
-	document.write("PM")
+	if(hours >= 13) {
+		hours = hours - 12
+		document.write(hours + ":" + minutes + " ")
+		document.write("PM")
+	} else if(hours > 11){
+		document.write("PM")
 	} else {
-	document.write("AM")
+		document.write("AM")
 	}
 	//-->
 	</script>
@@ -30,6 +31,5 @@
 		</div>
 		<div class="sp-circle-med"><br><br>Zoloft<br>11:30AM</div>
 		<div class="sp-circle-small"><br><br>Vicodin<br>9:00AM</div>
-		</div>	
 	</div><!-- /content -->
 <? include_once('footer.php'); ?>
